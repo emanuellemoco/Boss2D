914f9a5cc6f788f48d43d66a0893079aabdeae74
emanuellemoco <emanuellesmoco@gmail.com>
2021-04-01 17:08:58 -0300

    ataque do payer + wolf + cenario

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Tutorial para fazer o Jump: https://www.youtube.com/watch?v=ptvK4Fp5vRY
     public int velocity;
+
+    public LayerMask platformLayer;
+
+    public float attackDelay = 0.4f;
+    public Transform attackPoint; 
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    private float _attacktTimestamp = 0.0f;
     Animator animator;
     
-    
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D boxCollider;
     private bool isDead; 
-
 
     void Start()
     {
         animator = GetComponent<Animator>();
         isDead = false;
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
         
     }
+    bool isGrounded(){
+        RaycastHit2D rayCastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 1f, platformLayer);   
+    
+        return rayCastHit2D.collider != null;
+    }
+    
+
 
     // Update is called once per frame
+    void Update(){
+        //Arrumar para permitir pulo duplo e nao infinito.
+        if (isGrounded() && Input.GetKeyDown(KeyCode.Space)){
+            animator.SetBool("Jump", true);
+            float jumpVelocity = 5f; 
+            rigidBody.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+
     void FixedUpdate()
     {
         if (!isDead){
+        if (!isGrounded()) 
+            animator.SetBool("Jump", false);
         float inputX = Input.GetAxis("Horizontal");
         transform.position += new Vector3(inputX, 0, 0) * Time.deltaTime * velocity;
         
@@ -37,7 +66,35 @@ public class PlayerController : MonoBehaviour
         else
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            Attack();
         
+    }
+
+
+    void Attack(){
+        if ( Time.time - _attacktTimestamp < attackDelay) 
+            return;
+            
+        _attacktTimestamp = Time.time;
+        //Criar para os 3 tipos de ataques e chamar aleaoriamente
+        animator.SetTrigger("Attack");
+        
+        //Detectando os inimigos
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        
+        //Dano ao inimigo
+        foreach(Collider2D enemy in hitEnemies){
+            Debug.Log("Bati no inimigo");
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null )
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Boss2D
 2D game 
+
+
+Arrumar pulo
+Arrumar animação de ataque do player para ter os 3 tipos diferentes
+
+To do:
+Prefab de todos os personagens (inimigos)
+Comportamento dos inimigos
+UI (menu, pause, play)
+Mudar de cena no fim da tela 
+Montar os mapas
+Pontos e vidas
+Drops de comida para dar vida
+
+Só permitir avançar e nao voltar
